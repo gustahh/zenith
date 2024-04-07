1a861445cc77618c416f50c2be81b1a1e1d0ac41
John Doe <johndoe@email.com>
2024-04-07 16:43:45 -0300

    landing começo

--- frontend/zenithReact/src/pages/Index.jsx
+++ frontend/zenithReact/src/pages/Index.jsx
@@ -7,6 +7,41 @@ function Index() {
         <div>
             <h1>Index</h1>
         </div>
+
+        <section>
+          <div>
+            <h3>Registre seus momentos...</h3>
+
+          <div>
+            <img src=""></img>
+          </div>
+          <h2>"Anote seus pensamentos"</h2>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h2>"Monitore seu humor"</h2>
+          </div>
+
+          <div>
+            <img src=""></img>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <img src="" alt="" />
+          </div>
+
+          <div>
+            <h2>"Estipule suas metas"</h2>
+          </div>
+        </section>
+
+        <footer>
+          Footer
+        </footer>
     </>
   )
 }
