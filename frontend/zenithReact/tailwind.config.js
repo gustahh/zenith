73683c7ef0824125b23c56a77528cf29e3371bb4
gustahh <gustavocarlos206@gmail.com>
2024-04-07 15:24:58 -0300

    cores tailwind

--- frontend/zenithReact/tailwind.config.js
+++ frontend/zenithReact/tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}", ],
   theme: {
     extend: {},
+    colors: {
+      'verdeLimao' : '#D8E9A8',
+      'verde': '#4E9F3D',
+      'verdeEscuro': '#1E5128',
+      'preto': '#191A19'
+      
+    }
   },
   plugins: [],
 }
