dd2f2d098b2c42bd8ee8653794780723d1ddc0a4
Pablo <pablovitoraraujoazevedo@gmail.com>
2024-04-11 21:02:13 -0300

    barra lateral

--- frontend/zenithReact/src/pages/Anotacoes.jsx
+++ frontend/zenithReact/src/pages/Anotacoes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Anotacoes() {
   return (
     <div className='w-full h-screen bg-white dark:bg-cinzaEscuro'>
-      
+      <div className='w-96 h-screen bg-red-500'></div>
     </div>
   )
 }
