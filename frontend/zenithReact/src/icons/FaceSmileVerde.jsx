ce66f591950f48e0f440ef1303d8c129c79f1aeb
gustahh <gustavocarlos206@gmail.com>
2024-04-11 21:41:40 -0300

    icons adicionados

---
+++ frontend/zenithReact/src/icons/FaceSmileVerde.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function FaceSmileVerde(props) {
+	const title = props.title || "face smile 2";
+
+	return (
+		<svg height="18" width="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+	<title>{title}</title>
+	<g fill="#4e9f3d" stroke="#4e9f3d">
+		<circle cx="9" cy="9" fill="none" r="7.25" stroke="#4e9f3d" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
+		<circle cx="6" cy="9" r="1" stroke="none"/>
+		<circle cx="12" cy="9" r="1" stroke="none"/>
+		<path d="M11.25,11.758c-.472,.746-1.304,1.242-2.25,1.242s-1.778-.496-2.25-1.242" fill="none" stroke="inherit" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
+	</g>
+</svg>
+	);
+};
+
+export default FaceSmileVerde;
--- frontend/zenithReact/src/pages/Anotacoes.jsx
+++ frontend/zenithReact/src/pages/Anotacoes.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import BarraLateral from '../components/BarraLateral'
+import BookCinza from '../icons/BookCinza'
+import BookVerde from '../icons/BookVerde'
 
 function Anotacoes() {
   return (
     <div className='w-full h-screen bg-ice dark:bg-cinzaEscuro'>
      <BarraLateral/>
-     
-
-     
     </div>
   )
 }
