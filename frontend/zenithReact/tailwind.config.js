35b7a1c295611262d7f0a9b693afa1c3b2663e9b
gustahh <gustavocarlos206@gmail.com>
2024-04-11 21:03:23 -0300

    cinza adicionado

--- frontend/zenithReact/tailwind.config.js
+++ frontend/zenithReact/tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         'verde': '#4E9F3D',
         'verdeEscuro': '#1E5128',
         'cinzaEscuro': '#191A19',
+        'cinza': '#191919',
         'cinzaTexto': '#999999',
         'preto': '#000000' 
       }
