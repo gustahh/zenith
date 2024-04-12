70822d9b3ad961a93686f8cbd38c6ee259ccca69
Pablo <pablovitoraraujoazevedo@gmail.com>
2024-04-11 21:31:44 -0300

    barra lateral

---
+++ frontend/zenithReact/src/components/BarraLateral.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BarraLateral() {
+  return (
+    <div className='w-96 h-screen bg-ice dark:bg-cinza shadow-lg'></div>
+  )
+}
+
+export default BarraLateral
--- frontend/zenithReact/src/pages/Anotacoes.jsx
+++ frontend/zenithReact/src/pages/Anotacoes.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import BarraLateral from '../components/BarraLateral'
 
 function Anotacoes() {
   return (
     <div className='w-full h-screen bg-ice dark:bg-cinzaEscuro'>
-      <div className='w-96 h-screen bg-ice dark:bg-cinza'></div>
+     <BarraLateral/>
+     
+
+     
     </div>
   )
 }
