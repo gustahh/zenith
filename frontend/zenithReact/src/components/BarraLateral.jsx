0461d09c1e2b52d3b312d2c2c1d291ddab7c8cbf
Pablo <pablovitoraraujoazevedo@gmail.com>
2024-04-11 21:52:47 -0300

    link barra

--- frontend/zenithReact/src/components/BarraLateral.jsx
+++ frontend/zenithReact/src/components/BarraLateral.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function BarraLateral() {
   return (
-    <div className='w-96 h-screen bg-ice dark:bg-cinza shadow-lg'></div>
+    <div className='w-96 h-screen bg-ice dark:bg-cinza shadow-lg'>
+        <ul>
+            
+            <li>Notas</li> 
+            <Link to="/relatorio" ><li>Relatório</li></Link>
+            <li>Metas</li>
+        </ul>
+    </div>
   )
 }
 
