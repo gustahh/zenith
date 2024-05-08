4904d68668470ab2b59d40ec97449b6f104442ec
gustahh <gustavocarlos206@gmail.com>
2024-05-08 17:04:58 -0300

    exibir saudacao

---
+++ frontend/zenithReact/src/components/Cumprimento.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Cumprimento() {
+ function hora() {
+    let hora = new Date().getHours();
+
+    if (hora >= 5 && hora < 12) {
+        return 'Bom dia';
+    } else if (hora >= 12 && hora < 18) {
+        return 'Boa tarde';
+    } else if (hora >= 18 && hora < 24) {
+        return 'Boa noite';
+    } else {
+        return 'Boa madrugada';
+    }
+ }
+  return (
+    <div className='text-xl font-bold text-cinzaTexto'>{hora()}</div>
+  )
+}
+
+export default Cumprimento
--- frontend/zenithReact/src/pages/Home.jsx
+++ frontend/zenithReact/src/pages/Home.jsx
@@ -4,11 +4,13 @@ import View from '../components/View'
 import BookCinza from '../icons/BookCinza'
 import BookVerde from '../icons/BookVerde'
 import FraseDoDia from '../components/FraseDoDia'
+import Cumprimento from '../components/Cumprimento'
 
 function Home() {
     return (
         <>
             <View>
+                <Cumprimento />
                 <FraseDoDia />
             </View>
 
