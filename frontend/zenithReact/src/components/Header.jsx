db4e477912853cffe38bd27f6ec20f2b63d99f7a
Raphael Vieirae <raphael.v.freitas03@gmail.com>
2024-05-11 18:23:17 -0300

    fazendo a index

--- frontend/zenithReact/src/components/Header.jsx
+++ frontend/zenithReact/src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Button from '../components/Button';
 
 function Header() {
   return (
-    <div className=''>Header</div>
+    <header className='w-full h-16 bg-red-500 p-5 flex items-center'>
+      <h1 className='text-verde text-4xl font-bold'>Zenith</h1>
+      <Button/>
+    </header>
   )
 }
 
