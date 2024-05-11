543979c1bdba09921224986f8230da94de543fa1
Raphael Vieirae <raphael.v.freitas03@gmail.com>
2024-05-11 18:31:31 -0300

    continuando header

--- frontend/zenithReact/src/components/Button.jsx
+++ frontend/zenithReact/src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button(props) {
   let css = '';
   return (
-    <button type={props.type} className={`${props.width} ${props.height} text-white bg-verde rounded font-bold p-2.5 px-40`}>{ props.text }</button>
+    <button type={props.type} className={`${props.width} ${props.height} ${props.float} ${props.cor}  text-white  rounded-md font-bold p-2.5`}>{ props.text }</button>
   )
 }
 
--- frontend/zenithReact/src/components/Header.jsx
+++ frontend/zenithReact/src/components/Header.jsx
@@ -3,9 +3,9 @@ import Button from '../components/Button';
 
 function Header() {
   return (
-    <header className='w-full h-16 bg-red-500 p-5 flex items-center'>
+    <header className='w-full h-16 bg-red-500 p-5 flex items-center justify-stretch'>
       <h1 className='text-verde text-4xl font-bold'>Zenith</h1>
-      <Button/>
+      <Button float='float-right' width='auto' height='auto' text='Comece a usar gratuitamente' cor='bg-verde'/>
     </header>
   )
 }
