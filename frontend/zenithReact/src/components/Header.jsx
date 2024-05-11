461fa62bee8bf88ca27bf34ae52dd5d9fbe60681
Raphael Vieirae <raphael.v.freitas03@gmail.com>
2024-05-11 18:53:29 -0300

    links funcionando

--- frontend/zenithReact/src/components/Header.jsx
+++ frontend/zenithReact/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '../components/Button';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Header() {
   return (
@@ -8,10 +9,14 @@ function Header() {
         <h1 className='text-verde text-4xl font-bold ml-3'>Zenith</h1>
 
         <div className={`flex items-center ml-auto`}>
+        <Link to="/login">
           <Button className='w-auto h-auto p-2 bg-transparent rounded-md mr-2 text-verde 
       font-bold text-sm' text='Entrar' />
+        </Link> 
+      <Link to="/registrar">
           <Button className='w-auto h-auto p-2 bg-verde rounded-md mr-2 text-white 
       font-bold text-sm' text='Comece a usar gratuitamente' />
+      </Link> 
         </div>
       </div>
 
