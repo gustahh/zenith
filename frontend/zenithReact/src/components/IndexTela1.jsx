2c3c8f78184fdedcfbcefb7ab6b7315a14fbbda6
Raphael Vieirae <raphael.v.freitas03@gmail.com>
2024-05-11 19:18:16 -0300

    tentando resolver enquadramento

--- frontend/zenithReact/src/components/IndexTela1.jsx
+++ frontend/zenithReact/src/components/IndexTela1.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function IndexTela1() {
   return (
-    <div>IndexTela1</div>
+    <div className='w-full h-screen bg-[#F6F4EB] flex justify-center flex-wrap items-center'>
+      <div className='flex justify-center items-center'>
+        <h1 className='text-6xl font-bold pt-32'>Organize a sua vida.</h1>
+        <h3 className='text-3xl font-bold pt-5'>Organize seus pensamentos, sentimentos, rotina e
+  estipule as suas metas.
+        </h3>
+      </div>
+      </div>
   )
 }
 
