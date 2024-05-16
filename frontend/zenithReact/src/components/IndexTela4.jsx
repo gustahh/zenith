fc9c2cf6b74931b35f8cffc3a1fbdb15bebe8f26
Raphael Vieirae <raphael.v.freitas03@gmail.com>
2024-05-16 20:58:27 -0300

    página funcionando agora

--- frontend/zenithReact/src/components/IndexTela4.jsx
+++ frontend/zenithReact/src/components/IndexTela4.jsx
@@ -9,7 +9,7 @@ function IndexTela4() {
     <div className='w-full h-[90%] flex pb-10'>
       <div className='w-full flex flex-col justify-center items-stretch p-10'>
         <span className='font-bold p-10 text-3xl self-start'>Estipule suas metas </span>
-        <img className="w-5/12 h-5/12 pt-3 self-start" src={Tela1} alt="" />
+        <img className="w-5/12 h-5/12 pt-3 self-start" src={metas} alt="" />
         <span className='float-left p-20 text-3xl font-bold self-end'>Acompanhe seu humor de forma semanal e mensal</span>
       </div>
     </div>
