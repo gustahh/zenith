0de0afcbb6663b314fc223a1fddbd7010af6493b
Raphael Vieirae <raphael.v.freitas03@gmail.com>
2024-05-16 20:50:05 -0300

    tentando carregar tela 4 index

--- frontend/zenithReact/src/components/IndexTela4.jsx
+++ frontend/zenithReact/src/components/IndexTela4.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import metas from "../img/metas.png"
 import img2 from "../img/img2.png"
 
+
 function IndexTela4() {
   return (
-    <div>IndexTela4</div>
+    <>
+    <div className='w-full h-[90%] flex pb-10'>
+      <div className='w-full flex flex-col justify-center items-stretch p-10'>
+        <span className='font-bold p-10 text-3xl self-start'>Estipule suas metas </span>
+        <img className="w-5/12 h-5/12 pt-3 self-start" src={Tela1} alt="" />
+        <span className='float-left p-20 text-3xl font-bold self-end'>Acompanhe seu humor de forma semanal e mensal</span>
+      </div>
+    </div>
+  </>
   )
 }
 
