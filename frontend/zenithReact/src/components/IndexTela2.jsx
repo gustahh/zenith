b4862f6843312f0b1109ac18851dfd299d33b4fa
duraoraphael <duraoraphael06@gmail.com>
2024-05-15 23:42:28 -0300

    Feito pelo Raphael Durão

--- frontend/zenithReact/src/components/IndexTela2.jsx
+++ frontend/zenithReact/src/components/IndexTela2.jsx
@@ -8,8 +8,8 @@ function IndexTela2() {
         <div className='w-full flex flex-col justify-center items-stretch p-10'>
           <span className='font-bold p-10 text-3xl self-start'>Registre seus momentos... </span>
           <img className="w-5/12 h-5/12 pt-3 self-start" src={Tela1} alt="" />
-          <span className='float-left p-20 text-3xl font-bold self-end'>Acompanhe seu humor de forma semanal e mensal</span>
-        </div>
+          <span className='font-bold p-10 text-3xl self-end'>Registre seus momentos... </span>
+         </div>
       </div>
     </>
   )
--- frontend/zenithReact/src/components/IndexTela3.jsx
+++ frontend/zenithReact/src/components/IndexTela3.jsx
@@ -5,8 +5,9 @@ function IndexTela3() {
   return (
     <>
     <div className='w-full h-[90%] flex bg-[#F6F4EB] items-center '>
-      <div className='w-full pt-10 flex justify-end items-center p-10'>
-        <span className='float-left p-20 text-6xl font-bold'>Acompanhe seu humor de forma semanal e mensal</span>
+      <div className='w-full pt-10 flex justify-between items-center  p-10'>
+        <span className='font-bold p-10 text-4xl self-start'>Monitoramento de Humor</span>
+        <span className='font-bold p-10 text-1xl self-start'>Com o Zenith você pode registrar seus sentimentos diariamente de maneira simples e com a nossa tecnologia será analisada suas anotações e criado gráficos semanais e mensais que fornecerão insights valiosos sobre suas tendências emocionais. Os gráficos te ajudarão a identificar os humores tidos durante esse período, permitindo uma compreensão mais profunda de seu bem-estar emocional.</span>
         <img src={Relatorio} alt="" />
       </div>
     </div>
