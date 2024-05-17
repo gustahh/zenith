0e7be81e6b9749d1fd7d93d66c002d94b00d6f83
gustahh <gustavocarlos206@gmail.com>
2024-05-16 22:54:52 -0300

    index tela 3

--- frontend/zenithReact/src/components/IndexTela3.jsx
+++ frontend/zenithReact/src/components/IndexTela3.jsx
@@ -4,15 +4,25 @@ import Relatorio from "../img/relatorio.png"
 function IndexTela3() {
   return (
     <>
-    <div className='w-full h-[90%] flex bg-[#F6F4EB] items-center '>
-      <div className='w-full pt-10 flex justify-end items-center p-10'>
-        <span className='float-left p-20 text-6xl font-bold'>Acompanhe seu humor de forma semanal e mensal</span>
-        <img src={Relatorio} alt="" />
+      <div className='w-full h-[90%] bg-[#F6F4EB] flex justify-end items-center'>
+        <div className='w-full flex justify-start'>
+
+          <div className='w-full flex items-center'>
+            <div className='p-5'>
+              <span className='font-bold text-6xl text-justify'>Seus relatórios mensais e semanais </span>
+              <p className='text-xl'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+
+          </div>
+
+          <div className='w-full pl-10 pt-10 pb-10 flex items-center'>
+            <img className="w-30 h-30 pt-10" src={Relatorio} alt="" />
+          </div>
+        </div>
       </div>
-    </div>
-  </>
-)
+    </>
+  )
 }
-    
+
 
 export default IndexTela3
--- frontend/zenithReact/src/components/IndexTela4.jsx
+++ frontend/zenithReact/src/components/IndexTela4.jsx
@@ -13,7 +13,7 @@ function IndexTela4() {
           </div>
           <div className='w-full flex items-center'>
             <div className='p-5'>
-              <span className='font-bold text-6xl text-justify'>Estipule suas metas </span>
+              <span className='pb-2 font-bold text-6xl text-justify'>Estipule suas metas </span>
               <p className='text-xl'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
             </div>
 
