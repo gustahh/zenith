4e77fcb630dcae0b961e36c799369d0d7c808ea0
duraoraphael <duraoraphael06@gmail.com>
2024-05-16 23:02:37 -0300

    Mudança na tela IndexTela4 texto2

--- frontend/zenithReact/src/components/IndexTela4.jsx
+++ frontend/zenithReact/src/components/IndexTela4.jsx
@@ -14,7 +14,7 @@ function IndexTela4() {
           <div className='w-full flex items-center'>
             <div className='p-5'>
               <span className='pb-2 font-bold text-6xl text-justify'>Estipule suas metas </span>
-              <p className='text-xl'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+              <p className='text-xl'>Crie metas distintas para área da vida de sua escolha. Nosso enfoque com base em objetivos entrega a você uma finalidade clara a ser alcançada.</p>
             </div>
 
           </div>
