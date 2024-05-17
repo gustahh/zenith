07841538bc51d7469c47294057858cd93031d41e
pablo <pabloazevedo@souunisuam.com.br>
2024-05-16 21:40:07 -0300

    logout

--- frontend/zenithReact/src/components/BarraLateral.jsx
+++ frontend/zenithReact/src/components/BarraLateral.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import { Link, useLocation } from 'react-router-dom'
 import HouseVerde from "../icons/HouseVerde"
 import BookVerde from '../icons/BookVerde'
@@ -10,21 +9,11 @@ import HouseCinza from '../icons/HouseCinza'
 import FaceSmileCinza from '../icons/FaceSmileCinza'
 import TargetCinza from '../icons/TargetCinza'
 import Li from './Li'
-import Configuracoes from '../icons/Configuracoes';
-import Logout from '../icons/Logout';
+import UsuarioLogado from './UsuarioLogado'
 
 function BarraLateral() {
   const location = useLocation();
-  const [nome, setNome] = useState('');
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-      axios.get('http://localhost:3000/perfil')
-          .then((res) => {
-              setNome(res.data.results[0].nome);
-          });
-  }, []);
+  
   return (
     <div className='w-1/4 h-screen bg-ice dark:bg-cinza shadow-lg float-left flex flex-col'>
       <div className='flex-1'>
@@ -51,31 +40,9 @@ function BarraLateral() {
         </ul>
       </div>
 
-      <div className='w-full h-16 p-3 flex items-center justify-center 
-          font-bold container mx-auto'>
-        <div className='w-full h-full flex items-center justify-stretch'>
-          <div>
-            <div className='w-12 h-12 bg-black rounded-full float-left'></div>
-          </div>
+      <UsuarioLogado/>
 
-          <div>
-            <span className='text-xl font-bold text-cinzaTexto pl-3 float-left'>
-              {nome}
-            </span>
-          </div>
-
-          <button className='ml-20 float-right'>
-            <Configuracoes className="" />
-          </button>
-
-          <button className='ml-3 float-right'>
-            <Logout className="" />
-          </button>
-        </div>
-
-
-
-      </div>
+      
     </div>
   )
 }
---
+++ frontend/zenithReact/src/components/UsuarioLogado.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import Configuracoes from '../icons/Configuracoes';
+import Logout from '../icons/Logout';
+import axios from 'axios'
+
+
+function UsuarioLogado() {
+    const [nome, setNome] = useState('');
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      axios.get('http://localhost:3000/perfil')
+          .then((res) => {
+              setNome(res.data.results[0].nome);
+          });
+  }, []);
+
+  function Logout(){
+    localStorage.removeItem('token');
+
+
+  }
+  return (
+        <div className='w-full h-16 p-3 flex items-center justify-center 
+          font-bold container mx-auto'>
+        <div className='w-full h-full flex items-center justify-stretch'>
+          <div>
+            <div className='w-12 h-12 bg-black rounded-full float-left'></div>
+          </div>
+
+          <div>
+            <span className='text-xl font-bold text-cinzaTexto pl-3 float-left'>
+              {nome}
+            </span>
+          </div>
+
+          <button className='ml-20 float-right'>
+            <Configuracoes className="" />
+          </button>
+
+          <button className='ml-3 float-right' onClick={Logout}>
+            <Logout className="" />
+          </button>
+        </div>
+      </div>
+  )
+}
+
+export default UsuarioLogado
