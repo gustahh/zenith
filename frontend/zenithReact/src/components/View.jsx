9a28bdd58fb1687aecb7595cd321b23af145e39e
Gustavo Graciano <66232093+gustahh@users.noreply.github.com>
2024-05-29 10:41:36 -0300

    novas paginas

--- frontend/zenithReact/src/components/View.jsx
+++ frontend/zenithReact/src/components/View.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import BarraLateral from './BarraLateral'
 import BarraMobile from './BarraMobile'
 
+
 function View(props) {
     return (
         <>
-            
             <BarraLateral />
             <div className={`w-[83%] sm:w-[75%] h-screen bg-ice dark:bg-cinzaEscuro p-5 float-left overflow-y-scroll`}>
                 {props.children}
---
+++ frontend/zenithReact/src/components/ViewConfiguracao.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ViewConfiguracao(props) {
+    return (
+        <>
+            <div className={`w-[50%] sm:w-[50%] h-screen bg-red-500 dark:bg-cinzaEscuro p-5 float-left`}>
+                {props.children}
+            </div>
+        </>
+
+
+    )
+}
+
+export default ViewConfiguracao
