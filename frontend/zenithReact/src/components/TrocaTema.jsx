88fb6d1890ef6c118ee90e60f1c316be5325381e
Raphael Vieira <raphael.v.freitas03@gmail.com>
2024-06-19 09:44:33 -0300

    modo dark funcionando feat: pablo e durao e leo

--- frontend/zenithReact/src/components/TrocaTema.jsx
+++ frontend/zenithReact/src/components/TrocaTema.jsx
@@ -2,12 +2,24 @@ import React, { useState, useEffect } from 'react'
 
 function TrocaTema() {
 
+  const [opcaoSelecionada, setOpcaoSelecionada] = useState('');
+  const handleSelectChange = (event) => {
+    setOpcaoSelecionada(event.target.value);
+
+    window.location.reload();
+  };
+  if (opcaoSelecionada === 'dark') {
+    localStorage.setItem('tema', opcaoSelecionada);
+  } else if (opcaoSelecionada === 'light') {
+    localStorage.setItem('tema', opcaoSelecionada);
+  }
   
 
+  let selecionado = localStorage.getItem('tema');
   return (
-    <select name="trocatema" className='p-0 flex w-28 h-8 bg-preto/10 rounded mb-3 placeholder:text-cinzaTexto text-cinzaTexto'>
-      <option value="light">Modo Claro</option>
-      <option value="dark" >Modo Escuro</option>
+    <select value={selecionado} onChange={handleSelectChange} name="trocatema" className='p-0 flex w-28 h-8 bg-preto/10 rounded mb-3 placeholder:text-cinzaTexto text-cinzaTexto'  >
+      <option value="light">Claro</option>
+      <option value="dark" >Escuro</option>
     </select>
   )
 }
